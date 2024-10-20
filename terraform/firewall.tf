50cab9f2079b9277f637d12355c41b4248bca4ef
Helmet <ncarriage@gmail.com>
2024-10-20 13:49:36 +1100

    Update firewall.tf

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -32,54 +32,68 @@ resource "aws_vpc_security_group_ingress_rule" "https_inbound_my_ip" {
   ip_protocol       = "tcp"
 }
 
-ingress {
-    from_port   = 1514
-    to_port     = 1514
-    protocol    = "udp"
-    cidr_blocks = [var.my_ip]  # Replace with your actual IP or secret value
-  }
+resource "aws_vpc_security_group_ingress_rule" "wazuh_syslog_udp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Wazuh Syslog Inbound (UDP 1514) from my IP"
+  from_port         = 1514
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 1514
+  ip_protocol       = "udp"
+}
 
-  ingress {
-    from_port   = 1515
-    to_port     = 1515
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
+resource "aws_vpc_security_group_ingress_rule" "wazuh_protocol_tcp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Wazuh Protocol Inbound (TCP 1515) from my IP"
+  from_port         = 1515
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 1515
+  ip_protocol       = "tcp"
+}
 
-  ingress {
-    from_port   = 55000
-    to_port     = 55000
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
+resource "aws_vpc_security_group_ingress_rule" "wazuh_api_tcp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Wazuh API Inbound (TCP 55000) from my IP"
+  from_port         = 55000
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 55000
+  ip_protocol       = "tcp"
+}
 
-  ingress {
-    from_port   = 55000
-    to_port     = 55000
-    protocol    = "udp"
-    cidr_blocks = [var.my_ip]
-  }
+resource "aws_vpc_security_group_ingress_rule" "wazuh_api_udp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Wazuh API Inbound (UDP 55000) from my IP"
+  from_port         = 55000
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 55000
+  ip_protocol       = "udp"
+}
 
-  ingress {
-    from_port   = 9200
-    to_port     = 9200
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
+resource "aws_vpc_security_group_ingress_rule" "elasticsearch_tcp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Elasticsearch Inbound (TCP 9200) from my IP"
+  from_port         = 9200
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 9200
+  ip_protocol       = "tcp"
+}
 
-  ingress {
-    from_port   = 9300
-    to_port     = 9300
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
+resource "aws_vpc_security_group_ingress_rule" "elasticsearch_cluster_tcp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Elasticsearch Cluster Inbound (TCP 9300) from my IP"
+  from_port         = 9300
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 9300
+  ip_protocol       = "tcp"
+}
 
-  ingress {
-    from_port   = 5601
-    to_port     = 5601
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
+resource "aws_vpc_security_group_ingress_rule" "kibana_tcp_inbound_my_ip" {
+  security_group_id = aws_security_group.security_group.id
+  description       = "Kibana Inbound (TCP 5601) from my IP"
+  from_port         = 5601
+  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  to_port           = 5601
+  ip_protocol       = "tcp"
+}
 
 
 resource "aws_vpc_security_group_ingress_rule" "internal" {
